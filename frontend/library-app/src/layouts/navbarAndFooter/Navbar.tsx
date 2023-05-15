edec3f71c0b6b4eacedbeeb97fa330281387024e
Yurasevv <yurasev.work@mail.ru>
2023-05-15 21:07:02 +0300

    Minor navbar fix

--- frontend/library-app/src/layouts/navbarAndFooter/Navbar.tsx
+++ frontend/library-app/src/layouts/navbarAndFooter/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
             <div className="container-fluid">
-                <span className="navbar-brand">YuRead</span>
+                <span className="navbar-brand mb-0 h1">YuRead</span>
                 <button className="navbar-toggler" type="button"
                         data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                         aria-controls="navbarNavDropdown" aria-expanded="false"
