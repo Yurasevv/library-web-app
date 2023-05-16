daf049146b962df248d771ec1892daae06634bbe
Yurasevv <yurasev.work@mail.ru>
2023-05-16 10:25:23 +0300

    UI updates

--- frontend/library-app/src/layouts/homepage/ExploreTopBooks.tsx
+++ frontend/library-app/src/layouts/homepage/ExploreTopBooks.tsx
@@ -4,8 +4,8 @@ export const ExploreTopBooks = () => {
             <div className='container-fluid py-5 text-white
                 d-flex justify-content-center align-items-center'>
                 <div>
-                    <h1 className='display-5 fw-bold'>Spring into reading...</h1>
-                    <p className='col-md-8 fs-4'>Where would you like<br/> to go next?</p>
+                    <h1 className='display-4 fw-bold'>Spring into reading...</h1>
+                    <p className='col-md-8 fs-4'>Where would you like to go next?</p>
                     <a type='button' className='btn main-color btn-lg text-white' href='#'>
                         Best Sellers</a>
                 </div>
